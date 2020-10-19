cec084ad49f7bc544288e78ee4d8be93c30fa882
Tomas Alejandro Lugo Salinas <lugo.lbmi@gmail.com>
2020-10-19 15:10:01 -0500

    Update check_tests.sh
    adds echo for debugging

--- check_tests.sh
+++ check_tests.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
+echo one
 catkin_test_results > tmp_test_results.txt
-
+echo two
+cat tmp_test_results.txt
 lines_string=$(wc -l tmp_test_results.txt)
-
+echo three
 lines_array=(${lines_string// / })
 if [ ${lines_array[0]} -gt 1 ]
 then
