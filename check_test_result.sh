17b6de71bba156df05cdb50d8fb0e3d3735ff313
Tomas Alejandro Lugo Salinas <lugo.lbmi@gmail.com>
2020-10-19 15:33:39 -0500

    Update and rename check_tests.sh to check_test_result.sh
    remove echo and change name

--- check_test_result.sh
+++ check_test_result.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-echo one
+
 catkin_test_results > tmp_test_results.txt
-echo two
-cat tmp_test_results.txt
+
+
 lines_string=$(wc -l tmp_test_results.txt)
-echo three
+
 lines_array=(${lines_string// / })
 if [ ${lines_array[0]} -gt 1 ]
 then
-    echo Too many lines, it means something failed
+    echo [CHECK_TEST_RESULT] Too many lines, it means something failed
+    echo [CHECK_TEST_RESULT] resulting log:
+    cat tmp_test_results.txt
     exit 1
 else 
-    echo All good
+    echo [CHECK_TEST_RESULT] All good
 fi
 exit 0
